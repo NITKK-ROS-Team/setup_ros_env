27431a27c0dc6ad33f79ec6331d414500e9b7ad3
Ar-Ray-code <ray255ar@gmail.com>
2021-04-04 18:24:10 +0900

    add alias ros_env

--- autowrite_bashrc.sh
+++ autowrite_bashrc.sh
@@ -6,4 +6,8 @@ cp ./ros2_init.sh ~/.ros2_init.sh
 echo "if ! test -z \"\$ROS_DISTRO\"; then" >> ~/.bashrc
 echo "export PS1=\"(\$ROS_DISTRO):\[\033[01;34m\]\w\[\033[00m\]\$ \"" >> ~/.bashrc
 echo "fi" >> ~/.bashrc
+
+echo "alias ros1_init=\"source ~/.ros1_init.sh\"" >> ~/.bashrc
+echo "alias ros2_init=\"source ~/.ros2_init.sh\"" >> ~/.bashrc
+
 cd -
