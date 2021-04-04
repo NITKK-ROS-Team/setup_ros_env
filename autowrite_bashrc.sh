cfbb087161d8398a6913ce90c69359f54e9baf7a
Ar-Ray-code <ray255ar@gmail.com>
2021-04-04 18:01:37 +0900

    fix bug

--- autowrite_bashrc.sh
+++ autowrite_bashrc.sh
@@ -1,3 +1,9 @@
+#!/bin/sh
+cd `dirname $0`
+cp ./ros1_init.sh ~/.ros1_init.sh
+cp ./ros2_init.sh ~/.ros2_init.sh
+
 echo "if ! test -z \"\$ROS_DISTRO\"; then" >> ~/.bashrc
 echo "export PS1=\"(\$ROS_DISTRO):\[\033[01;34m\]\w\[\033[00m\]\$ \"" >> ~/.bashrc
 echo "fi" >> ~/.bashrc
+cd -
